02f41ce69386d69837739e401859b836bcf5f19e
slapstickers <maxgeiringer@knights.ucf.edu>
2024-03-04 18:01:36 -0500

    3/4 update

---
+++ Pair Programming/Assets/EnemyMove.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    public GameObject pointA;
+    public GameObject pointB;
+    public float speed;
+    private Rigidbody2D rigbod;
+    private Animator anim;
+    private Transform currentPoint;
+    void Start()
+    {
+        rigbod = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        currentPoint = pointB.transform;
+        anim.SetBool("isMoving", true);
+    }
+
+
+    void Update()
+    {
+        Vector2 point = currentPoint.position - transform.position;
+        if(currentPoint == pointB.transform)
+        {
+            rigbod.velocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            rigbod.velocity = new Vector2(-speed, 0);
+        }
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
+        {
+            flip();
+            currentPoint = pointA.transform;
+        }
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
+        {
+            flip();
+            currentPoint = pointB.transform;
+        }
+    }
+
+    private void flip()
+    {
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(pointA.transform.position, 0.5f);
+        Gizmos.DrawWireSphere(pointB.transform.position, 0.5f);
+    }
+}
